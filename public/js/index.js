d70f2f5c6070cd4847a9dc733844699a19a89a06
arun kumar <arun.devpro@gmail.com>
2024-09-10 17:15:51 +0530

    edit employee css and html markup

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,26 @@
 document.getElementById("hamburger").addEventListener("click", function () {
   this.classList.toggle("open");
 });
+
+const modal = document.getElementById('editModal');
+    const editButtons = document.querySelectorAll('.edit-btn');
+    const cancelBtn = document.getElementById('cancelBtn');
+
+    // When any edit button is clicked, show the modal
+    editButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        modal.style.display = 'flex'; // Show the modal
+      });
+    });
+
+    // When the cancel button is clicked, hide the modal
+    cancelBtn.addEventListener('click', () => {
+      modal.style.display = 'none'; // Hide the modal
+    });
+
+    // Optionally close the modal by clicking outside of it
+    window.onclick = function (event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    };
