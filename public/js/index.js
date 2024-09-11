02d907f6984cfe9856387ef5307bf1a8421052ae
arun kumar <arun.devpro@gmail.com>
2024-09-11 11:05:01 +0530

    flash message markup added

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,26 @@ document.getElementById("hamburger").addEventListener("click", function () {
   this.classList.toggle("open");
 });
 
-const modal = document.getElementById('editModal');
-    const editButtons = document.querySelectorAll('.edit-btn');
-    const cancelBtn = document.getElementById('cancelBtn');
+//edit modal
+const editModal = document.getElementById("editModal");
+const editButtons = document.querySelectorAll(".edit-btn");
+const cancelBtn = document.getElementById("cancelBtn");
 
-    // When any edit button is clicked, show the modal
-    editButtons.forEach((button) => {
-      button.addEventListener('click', () => {
-        modal.style.display = 'flex'; // Show the modal
-      });
-    });
+// When any edit button is clicked, show the modal
+editButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    editModal.style.display = "flex"; // Show the modal
+  });
+});
 
-    // When the cancel button is clicked, hide the modal
-    cancelBtn.addEventListener('click', () => {
-      modal.style.display = 'none'; // Hide the modal
-    });
+// When the cancel button is clicked, hide the modal
+cancelBtn.addEventListener("click", () => {
+  editModal.style.display = "none"; // Hide the modal
+});
 
-    // Optionally close the modal by clicking outside of it
-    window.onclick = function (event) {
-      if (event.target === modal) {
-        modal.style.display = 'none';
-      }
-    };
+// Optionally close the modal by clicking outside of it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    editModal.style.display = "none";
+  }
+};
