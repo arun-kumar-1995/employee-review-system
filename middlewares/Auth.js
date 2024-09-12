9ce5c19d2ecb2dcd9073a5ca32ffe5bee7caf194
arun kumar <arun.devpro@gmail.com>
2024-09-12 15:22:34 +0530

    added local user variable globally

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -4,8 +4,12 @@ const checkAuth = (req, res, next) => {
   if (_session) {
     cookie_data = JSON.parse(_session);
   }
+  // define authenticate function
   req.isAuthenticated = () => !!cookie_data?.token;
+  // assign user from cookie to req.user
   req.user = cookie_data?.user;
+  // so that we can access to each views
+  res.locals.user = req.user;
   next();
 };
 
